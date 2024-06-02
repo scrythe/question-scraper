53fc62ec59c172fc41c1279437dd3470558b6fc7
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-06-02 17:26:49 +0200

    feat: add method to scrape and save questions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,55 @@
-fn main() {
-    let response = reqwest::blocking::get("_bla_url_");
-    let html_content = response.unwrap().text().unwrap();
-    let document = scraper::Html::parse_document(&html_content);
-    let html_questions_selector = scraper::Selector::parse("span.notable-td.prompt").unwrap();
-    let html_questions = document.select(&html_questions_selector);
-    for question in html_questions {
-        println!("{:?}", question)
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufWriter, Write},
+};
+
+use reqwest::Client;
+use scraper::Html;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let document = get_document().await?;
+    let questions_part1 = get_questions(&document, 1).await?;
+    let questions_part2 = get_questions(&document, 2).await?;
+    let questions = vec![questions_part1, questions_part2];
+    write_questions_to_file(questions).await?;
+    Ok(())
+}
+
+async fn get_document() -> Result<Html, Box<dyn Error>> {
+    let client = Client::new();
+    let res = client
+        .get("https://{some-adhd-website-with-questions}")
+        .header(
+            "User-Agent",
+            "Thunder Client (https://www.thunderclient.com)",
+        )
+        .send()
+        .await?;
+    let html_content = res.text().await?;
+    let document = Html::parse_document(&html_content);
+    Ok(document)
+}
+
+async fn get_questions(document: &scraper::Html, part: i32) -> Result<Vec<String>, Box<dyn Error>> {
+    let questions_selector = format!(
+        "div.notable-table.vertical:nth-child({part}) span.notable-td.prompt > span:nth-child(2)"
+    );
+    let questions_selector = scraper::Selector::parse(&questions_selector).unwrap();
+    let questions_html = document.select(&questions_selector);
+    let mut questions: Vec<String> = vec![];
+    for question in questions_html {
+        let question_text = question.inner_html();
+        questions.push(question_text);
     }
+    Ok(questions)
+}
+
+async fn write_questions_to_file(questions: Vec<Vec<String>>) -> Result<(), Box<dyn Error>> {
+    let file = File::create("questions.json")?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &questions)?;
+    writer.flush()?;
+    Ok(())
 }
